c51dd6d94d8d7265f90602ccb1b17ea4bd415f21
jamesw1892 <gingersemaj1892@gmail.com>
2023-09-09 15:15:09 +0100

    Fix test grid naming

--- Test.py
+++ Test.py
@@ -10,14 +10,14 @@ import Util
 import unittest
 
 # Valid 3x3
-INIT_1 = [
+GRID_1 = [
     [1, 0, 1],
     [0, 0, 1],
     [1, 0, 1]
 ]
 
 # Valid 15x15
-INIT_2 = [
+GRID_2 = [
     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
     [1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
@@ -37,34 +37,34 @@ INIT_2 = [
 
 class TestGame(unittest.TestCase):
     def test_init_1(self):
-        Game.Ocean(INIT_1)
+        Game.Ocean(GRID_1)
 
     def test_width_1(self):
-        self.assertEqual(Game.Ocean(INIT_1).getWidth(), 3)
+        self.assertEqual(Game.Ocean(GRID_1).getWidth(), 3)
 
     def test_height_1(self):
-        self.assertEqual(Game.Ocean(INIT_1).getHeight(), 3)
+        self.assertEqual(Game.Ocean(GRID_1).getHeight(), 3)
 
     def test_rowNums_1(self):
-        self.assertEqual(Game.Ocean(INIT_1).getRowNums(), [2, 1, 2])
+        self.assertEqual(Game.Ocean(GRID_1).getRowNums(), [2, 1, 2])
 
     def test_colNums_1(self):
-        self.assertEqual(Game.Ocean(INIT_1).getColNums(), [2, 0, 3])
+        self.assertEqual(Game.Ocean(GRID_1).getColNums(), [2, 0, 3])
 
     def test_init_2(self):
-        Game.Ocean(INIT_2)
+        Game.Ocean(GRID_2)
 
     def test_width_2(self):
-        self.assertEqual(Game.Ocean(INIT_2).getWidth(), 15)
+        self.assertEqual(Game.Ocean(GRID_2).getWidth(), 15)
 
     def test_height_2(self):
-        self.assertEqual(Game.Ocean(INIT_2).getHeight(), 15)
+        self.assertEqual(Game.Ocean(GRID_2).getHeight(), 15)
 
     def test_rowNums_2(self):
-        self.assertEqual(Game.Ocean(INIT_2).getRowNums(), [3, 2, 1, 1, 4, 1, 3, 8, 1, 2, 0, 1, 1, 5, 2])
+        self.assertEqual(Game.Ocean(GRID_2).getRowNums(), [3, 2, 1, 1, 4, 1, 3, 8, 1, 2, 0, 1, 1, 5, 2])
 
     def test_colNums_2(self):
-        self.assertEqual(Game.Ocean(INIT_2).getColNums(), [4, 2, 1, 2, 2, 2, 2, 1, 4, 4, 5, 2, 1, 2, 1])
+        self.assertEqual(Game.Ocean(GRID_2).getColNums(), [4, 2, 1, 2, 2, 2, 2, 1, 4, 4, 5, 2, 1, 2, 1])
 
 class TestUtil(unittest.TestCase):
     def test_pad_centre_not_required(self):
@@ -79,5 +79,5 @@ class TestUtil(unittest.TestCase):
         self.assertEqual(Util.padCentre("hi", 1), "hi")
 
 if __name__ == "__main__":
-    # Solvers.solve(Solvers.dummySolver, Game.Ocean(INIT_2))
+    # Solvers.solve(Solvers.dummySolver, Game.Ocean(GRID_2))
     unittest.main()
